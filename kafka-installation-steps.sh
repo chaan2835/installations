0b2868894b7b80e0aed8fd43602c8e3793ab5e6e
chaan2835 <92902854+chaan2835@users.noreply.github.com>
2023-02-02 17:19:52 +0530

    kafka installation steps added

---
+++ kafka-installation-steps.sh
@@ -0,0 +1,15 @@
+apt-get update
+
+sudo apt install openjdk-8-jdk -y
+
+apt install wget -y
+
+wget https://archive.apache.org/dist/kafka/2.8.0/kafka_2.13-2.8.0.tgz
+
+apt install tar -y
+
+tar -xvf kafka_2.13-2.8.0.tgz
+
+mv kafka_2.13-2.8.0 kafka
+
+rm -rf kafka_2.13-2.8.0
